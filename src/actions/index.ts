03f052101b5814a3883590124601ce5ed792f3b5
sukebon <39957486+sukebon@users.noreply.github.com>
2024-01-16 23:38:02 +0900

    relavildate

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,4 +1,5 @@
 'use server';
+import { revalidatePath } from 'next/cache'
 import { db } from '@/db';
 import { redirect } from 'next/navigation';
 
@@ -7,7 +8,7 @@ export async function editSnippet(id: number, code: string) {
     where: { id },
     data: { code }
   });
-
+  revalidatePath(`/snippets/${id}`)
   redirect(`/snippets/${id}`);
 }
 
@@ -15,7 +16,51 @@ export async function deleteSnippet(id: number) {
   await db.snippet.delete({
     where: { id }
   });
+  revalidatePath('/')
   redirect('/');
 }
 
+
+export async function createSnippet(formState: { message: string; }, formData: FormData) {
+
+  // Check the user's inputs and make sure they're valid
+  const title = formData.get('title') ;
+  const code = formData.get('code');
+  try {
+
+    if (typeof title !== 'string' || title.length < 3) {
+      return {
+        message: 'Title must be longer'
+      }
+    }
+  
+    if (typeof code !== 'string' || code.length < 10) {
+      return {
+       message: 'Code must be longer'
+     } 
+    }
+  
+    // Create ea new record in the database
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code
+      }
+    });
+    
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        message: err.message
+      }
+    } else {
+      return {
+        message:'Something went wrong'
+      }
+    }
+  }
+  revalidatePath('/')
+  // Redirect the user back to the root route
+  redirect('/');
+}
 
--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -37,3 +37,13 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
     </pre>
   </div>;
 }
+
+export async function generateStaticParams() {
+  const snippets = await db.snippet.findMany();
+
+  return snippets.map((snippet) => {
+    return {
+      id: snippet.id.toString()
+    };
+  });
+}
